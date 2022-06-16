0831b253340354bfce52c32e8cb838652dcb284b
ris0323 <s1079s1079@gmail.com>
2022-06-16 15:35:46 +0800

    first commit

---
+++ precache-manifest.f84f9be303636a6514f7302c45e1c46e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cded741389cf016df99b",
+    "url": "/ris0323/resume_Portfolio/static/css/main.4df5d210.chunk.css"
+  },
+  {
+    "revision": "cded741389cf016df99b",
+    "url": "/ris0323/resume_Portfolio/static/js/main.cded7413.chunk.js"
+  },
+  {
+    "revision": "4d8f7170bd2a5518b906",
+    "url": "/ris0323/resume_Portfolio/static/js/runtime~main.4d8f7170.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/ris0323/resume_Portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "eb322c202779ae0f6b5fe174c1f91773",
+    "url": "/ris0323/resume_Portfolio/index.html"
+  }
+];
