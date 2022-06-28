40eff12e7f4eccf637a50a7c5a9cc075854e3019
ris0323 <s1079s1079@gmail.com>
2022-06-28 17:26:29 +0800

    Updates

---
+++ precache-manifest.39d7b4dca78e0f387f20a3bd474531ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eb88e009c1a6a68b5ee",
+    "url": "/profile-app/static/css/main.a63771e9.chunk.css"
+  },
+  {
+    "revision": "8eb88e009c1a6a68b5ee",
+    "url": "/profile-app/static/js/main.8eb88e00.chunk.js"
+  },
+  {
+    "revision": "14a461050cc2228292c8",
+    "url": "/profile-app/static/js/runtime~main.14a46105.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/profile-app/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "7fbd5dcdebe5fcc63c61e8898c6e0d80",
+    "url": "/profile-app/index.html"
+  }
+];
